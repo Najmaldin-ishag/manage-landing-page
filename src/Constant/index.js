37acac61576eaa665e83778662d7c9409bb92f4a
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-19 23:26:45 +0200

    added index.js

---
+++ src/Constant/index.js
@@ -0,0 +1,82 @@
+export const NavLinks = [
+  {
+    id: 1,
+    title: "Pricing",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Product",
+    url: "/pricing",
+  },
+  {
+    id: 3,
+    title: "About Us",
+    url: "/product",
+  },
+  {
+    id: 4,
+    title: "Careers;",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Community;",
+    url: "/about",
+  },
+];
+
+export const features = [
+  {
+    id: "01",
+    title: "Track company-wide progress",
+    description:
+      "See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way down to the smallest of details. Never lose sight of the bigger picture again.",
+  },
+  {
+    id: "02",
+    title: "Advanced built-in reports",
+    description:
+      "Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+  },
+  {
+    id: "03",
+    title: "Everything you need in one place",
+    description:
+      "Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+  },
+];
+
+const testimonials = [
+  {
+    imgPath: "/images/avatar-anisha.png",
+    name: "Anisha Li",
+    quote:
+      "Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.",
+  },
+  {
+    imgPath: "/images/avatar-ali.png",
+    name: "Ali Bravo",
+    quote:
+      "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.",
+  },
+  {
+    imgPath: "/images/avatar-richard.png",
+    name: "Richard Watts",
+    quote:
+      "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!",
+  },
+  {
+    imgPath: "/images/avatar-shanai.png",
+    name: "Shanai Gough",
+    quote:
+      "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.",
+  },
+];
+
+export const footerLinks = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "Pricing", path: "/pricing" },
+  { id: 3, name: "Products", path: "/products" },
+  { id: 4, name: "About Us", path: "/about" },
+];
