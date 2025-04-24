1bce8cdafa6409d3a527440cee835b12326a7562
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-25 01:59:51 +0300

    new updates to  the footer

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -2,11 +2,12 @@ import { FooterIcons, footerLinks1, footerLinks2 } from "../Constant";
 // const img = "/public/images/logo.svg";
 
 const date = new Date().getFullYear();
+
 const Footer = () => {
   return (
-    <footer className="py-[5rem] px-[2.5rem] bg-primary-dark-blue">
-      <div className="grid grid-cols-4 gap-5 items-center justify-center">
-        <div>
+    <footer className="py-[5rem] px-[2.5rem] bg-primary-dark-blue max-sm:overflow-hidden">
+      <div className="grid grid-cols-4 gap-5 items-center justify-center max-sm:grid-cols-1 max-sm:text-center ">
+        <div className="max-sm:row-start-4 max-sm:row-end-5 max-sm:mx-auto">
           <a href="/">
             <img src="/images/logo.svg" alt="Logo" className="mb-[2rem]" />
           </a>
@@ -17,11 +18,14 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="grid gap-2 text-white">
+        <div className="gap-2 grid text-white">
           {footerLinks1.map((link) => (
             <ul key={link.id}>
-              <li>
-                <a className="cursor-pointer" href="#">
+              <li className="">
+                <a
+                  className="cursor-pointer hover:text-primary-bright-red"
+                  href="#"
+                >
                   {link.name}
                 </a>
               </li>
@@ -29,11 +33,14 @@ const Footer = () => {
           ))}
         </div>
 
-        <div className="grid gap-2 text-white">
+        <div className=" text-white">
           {footerLinks2.map((link) => (
             <ul key={link.id}>
-              <li>
-                <a className="cursor-pointer" href="#">
+              <li className="mb-2">
+                <a
+                  className="cursor-pointer hover:text-primary-bright-red"
+                  href="#"
+                >
                   {link.name}
                 </a>
               </li>
@@ -41,12 +48,13 @@ const Footer = () => {
           ))}
         </div>
 
-        <div>
+        <div className="max-sm:row-span-full ">
           <form>
             <input
               className="bg-white py-2 px-8 rounded-full outline-0"
               type="text"
               placeholder="update in your inbox"
+              required
             />
             <button
               type="submit"
@@ -56,11 +64,15 @@ const Footer = () => {
             </button>
           </form>
 
-          <p className="text-white text-sm mt-8">
+          <p className="text-white text-sm mt-8 max-sm:hidden">
             Copyright {date} Designed by Front-end Mentor, Developed by
             Najmaldin Ishag
           </p>
         </div>
+        <p className="text-white text-sm mt-8 lg:hidden md:hidden">
+          Copyright {date} Designed by Front-end Mentor, Developed by Najmaldin
+          Ishag
+        </p>
       </div>
     </footer>
   );
