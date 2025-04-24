349b38d93516530d696dca4ffcc81d1cf9c5d069
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-04-24 23:04:46 +0200

    made the cta mobile reponsive

--- src/Sections/Cta.jsx
+++ src/Sections/Cta.jsx
@@ -2,8 +2,8 @@ import Button from "../Components/Button";
 
 const Cta = () => {
   return (
-    <section className="section-cta ">
-      <div className="grid grid-cols-2 gap-10 items-center justify-center container ">
+    <section className="section-cta max-sm:overflow-hidden">
+      <div className="grid grid-cols-2 gap-10 items-center justify-center container max-sm:flex max-sm:flex-col  max-sm:text-center ">
         <p className="text-white text-5xl font-bold">
           Simplify how your team works today.
         </p>
